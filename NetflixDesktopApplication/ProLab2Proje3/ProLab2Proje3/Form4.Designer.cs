98f9a0c2e83dfbde957ae34c237b28f3c00b6e13
hrnbykbs <hrndarkcss@gmail.com>
2021-03-01 19:40:08 +0300

    summary

---
+++ NetflixDesktopApplication/ProLab2Proje3/ProLab2Proje3/Form4.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace ProLab2Proje3
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form4));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(13, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 14;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(16, 67);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(190, 260);
+            this.pictureBox4.TabIndex = 15;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Crimson;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button3.Location = new System.Drawing.Point(722, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(78, 35);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Ana Menü";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(127, 35);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.ForeColor = System.Drawing.Color.White;
+            this.radioButton1.Location = new System.Drawing.Point(186, 9);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(68, 17);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ada göre";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ForeColor = System.Drawing.Color.White;
+            this.radioButton2.Location = new System.Drawing.Point(260, 9);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Türe göre";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox1.Location = new System.Drawing.Point(337, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(338, 29);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox2.BackgroundImage = global::ProLab2Proje3.Properties.Resources.searchikon2;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(644, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(28, 25);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 35);
+            this.panel1.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Crimson;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button1.Location = new System.Drawing.Point(69, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "İzle";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form4";
+            this.Text = "Netflix";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form4_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ NetflixDesktopApplication/ProLab2Proje3/ProLab2Proje3/kullanicisinifi.cs
@@ -0,0 +1,441 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProLab2Proje3
+{
+    public static class kullanicisinifi
+    {
+        public static int kullaniciid;
+        public static string kullaniciad;
+        public static string tercih1;
+        public static string tercih2;
+        public static string tercih3;
+        public static string film1;
+        public static string film2;
+        public static string film3;
+        public static string film4;
+        public static string film5;
+        public static string film6;
+        public static string film1ad;
+        public static string film2ad;
+        public static string film3ad;
+        public static string film4ad;
+        public static string film5ad;
+        public static string film6ad;
+        public static int film1id;
+        public static int film2id;
+        public static int film3id;
+        public static int film4id;
+        public static int film5id;
+        public static int film6id;
+        public static int saat;
+        public static int dakika;
+        public static int saniye;
+        public static int izlenenfilmid;
+        public static int arananfilmid;
+        public static string arananfilmad;
+        public static string arananfilmresim;
+        public static int bolumsayisi;
+        public static int kalinanbolum;
+
+
+
+        public static int KullaniciID
+        {
+            get
+            {
+                return kullaniciid;
+            }
+            set
+            {
+                kullaniciid = value;
+            }
+        }
+
+        public static string KullaniciAd
+        {
+            get
+            {
+                return kullaniciad;
+            }
+            set
+            {
+                kullaniciad = value;
+            }
+        }
+
+        public static string Tercih1
+        {
+            get
+            {
+                return tercih1;
+            }
+            set
+            {
+                tercih1 = value;
+            }
+        }
+
+        public static string Tercih2
+        {
+            get
+            {
+                return tercih2;
+            }
+            set
+            {
+                tercih2 = value;
+            }
+        }
+
+        public static string Tercih3
+        {
+            get
+            {
+                return tercih3;
+            }
+            set
+            {
+                tercih3 = value;
+            }
+        }
+
+        public static string Film1
+        {
+            get
+            {
+                return film1;
+            }
+            set
+            {
+                film1 = value;
+            }
+        }
+
+        public static string Film2
+        {
+            get
+            {
+                return film2;
+            }
+            set
+            {
+                film2 = value;
+            }
+        }
+
+        public static string Film3
+        {
+            get
+            {
+                return film3;
+            }
+            set
+            {
+                film3 = value;
+            }
+        }
+
+        public static string Film4
+        {
+            get
+            {
+                return film4;
+            }
+            set
+            {
+                film4 = value;
+            }
+        }
+
+        public static string Film5
+        {
+            get
+            {
+                return film5;
+            }
+            set
+            {
+                film5 = value;
+            }
+        }
+
+        public static string Film6
+        {
+            get
+            {
+                return film6;
+            }
+            set
+            {
+                film6 = value;
+            }
+        }
+
+        public static string Film1Ad
+        {
+            get
+            {
+                return film1ad;
+            }
+            set
+            {
+                film1ad = value;
+            }
+        }
+
+        public static string Film2Ad
+        {
+            get
+            {
+                return film2ad;
+            }
+            set
+            {
+                film2ad = value;
+            }
+        }
+        public static string Film3Ad
+        {
+            get
+            {
+                return film3ad;
+            }
+            set
+            {
+                film3ad = value;
+            }
+        }
+
+        public static string Film4Ad
+        {
+            get
+            {
+                return film4ad;
+            }
+            set
+            {
+                film4ad = value;
+            }
+        }
+        public static string Film5Ad
+        {
+            get
+            {
+                return film5ad;
+            }
+            set
+            {
+                film5ad = value;
+            }
+        }
+
+        public static string Film6Ad
+        {
+            get
+            {
+                return film6ad;
+            }
+            set
+            {
+                film6ad = value;
+            }
+        }
+        public static int Film1ID
+        {
+            get
+            {
+                return film1id;
+            }
+            set
+            {
+                film1id = value;
+            }
+        }
+
+        public static int Film2ID
+        {
+            get
+            {
+                return film2id;
+            }
+            set
+            {
+                film2id = value;
+            }
+        }
+        public static int Film3ID
+        {
+            get
+            {
+                return film3id;
+            }
+            set
+            {
+                film3id = value;
+            }
+        }
+
+        public static int Film4ID
+        {
+            get
+            {
+                return film4id;
+            }
+            set
+            {
+                film4id = value;
+            }
+        }
+
+        public static int Film5ID
+        {
+            get
+            {
+                return film5id;
+            }
+            set
+            {
+                film5id = value;
+            }
+        }
+
+        public static int Film6ID
+        {
+            get
+            {
+                return film6id;
+            }
+            set
+            {
+                film6id = value;
+            }
+        }
+        public static int Saat
+        {
+            get
+            {
+                return saat;
+            }
+            set
+            {
+                saat = value;
+            }
+        }
+
+        public static int Dakika
+        {
+            get
+            {
+                return dakika;
+            }
+            set
+            {
+                dakika = value;
+            }
+        }
+
+        public static int Saniye
+        {
+            get
+            {
+                return saniye;
+            }
+            set
+            {
+                saniye = value;
+            }
+        }
+
+        public static int IzlenenFilmID
+        {
+            get
+            {
+                return izlenenfilmid;
+            }
+            set
+            {
+                izlenenfilmid = value;
+            }
+        }
+
+        public static int ArananFilmID
+        {
+            get
+            {
+                return arananfilmid;
+            }
+            set
+            {
+                arananfilmid = value;
+            }
+        }
+        public static string ArananFilmAd
+        {
+            get
+            {
+                return arananfilmad;
+            }
+            set
+            {
+                arananfilmad = value;
+            }
+        }
+        public static string ArananFilmResim
+        {
+            get
+            {
+                return arananfilmresim;
+            }
+            set
+            {
+                arananfilmresim = value;
+            }
+        }
+
+        public static int BolumSayisi
+        {
+            get
+            {
+                return bolumsayisi;
+            }
+            set
+            {
+                bolumsayisi = value;
+            }
+        }
+
+        public static int KalinanBolum
+        {
+            get
+            {
+                return kalinanbolum;
+            }
+            set
+            {
+                kalinanbolum = value;
+            }
+        }
+
+        /*public kullanicisinifi()
+        {
+            Kullanici = "kimse";
+        }
+
+        public kullanicisinifi(string kullanici)
+        {
+            Kullanici = kullanici;
+        }
+
+        public string Kullanici { get; }
+
+        public override string ToString()
+        {
+            return Kullanici;
+        }*/
+    }
+}
